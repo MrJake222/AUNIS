19181cc3e4b9b0f6be0e1a923ac1b25d24f66843
MrJake222 <norbertzpilicy@gmail.com>
2018-10-04 12:51:51 +0200

    Added drops to ore and one crafting

--- src/main/java/mrjake/aunis/block/NaquadahOreBlock.java
+++ src/main/java/mrjake/aunis/block/NaquadahOreBlock.java
@@ -1,11 +1,29 @@
 package mrjake.aunis.block;
 
+import java.util.Random;
+
+import mrjake.aunis.item.AunisItems;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 
 public class NaquadahOreBlock extends BlockBase {
 	
 	public NaquadahOreBlock() {
 		super(Material.ROCK, SoundType.STONE, "naquadah_ore");
+		
+		setHardness(1.5f);
+		setHarvestLevel("pickaxe", 3);
+	}
+	
+	@Override
+	public int quantityDropped(Random random) {
+		return 3 + random.nextInt(2);
+	}
+	
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		return AunisItems.naquadahOreShard;
 	}
 }
