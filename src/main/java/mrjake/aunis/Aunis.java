4cdd475260844942dc29ad8d6e6e5cebb287b69e
MrJake222 <norbertzpilicy@gmail.com>
2020-02-05 11:14:09 +0100

    Changed version

--- src/main/java/mrjake/aunis/Aunis.java
+++ src/main/java/mrjake/aunis/Aunis.java
@@ -25,7 +25,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class Aunis {	
     public static final String ModID = "aunis";
     public static final String Name = "AUNIS";
-    public static final String Version = "1.6.0-alpha";
+    public static final String Version = "1.6.0-beta";
 
     public static final String MCVersion = "[1.12.2]";
  
