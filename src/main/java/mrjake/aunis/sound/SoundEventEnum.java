1e7a768a3271c6685b3132d0c30a8cc5b36af995
Glen Jolley <glen.jolley@engageltd.co.uk>
2020-05-13 20:43:45 +0100

    Audio rework
    Normalized gate sounds to be a little more pleasant on the ears at full
    volume.
    Moved sound attenuation distance code into the SoundHelper.
    Increased default gate audio distance for MW and U gates from 15 blocks
    to 20.

--- src/main/java/mrjake/aunis/sound/SoundEventEnum.java
+++ src/main/java/mrjake/aunis/sound/SoundEventEnum.java
@@ -22,25 +22,25 @@ public enum SoundEventEnum implements EnumKeyInterface<Integer> {
 	DHD_MILKYWAY_PRESS(10, "dhd_milkyway_press", 0.3f),
 	DHD_MILKYWAY_PRESS_BRB(11, "dhd_milkyway_press_brb", 0.3f),
 	
-	GATE_MILKYWAY_OPEN(12, "gate_milkyway_open", 1.0f * (float)AunisConfig.stargateConfig.audioRangeMultiplier),
-	GATE_MILKYWAY_CLOSE(13, "gate_milkyway_close", 1.0f * (float)AunisConfig.stargateConfig.audioRangeMultiplier),
-	GATE_MILKYWAY_DIAL_FAILED(14, "gate_milkyway_dial_fail", 0.3f),
-	GATE_MILKYWAY_DIAL_FAILED_COMPUTER(15, "gate_milkyway_dial_fail_computer", 1.5f),
-	GATE_MILKYWAY_INCOMING(16, "gate_milkyway_incoming", 1.0f),
+	GATE_MILKYWAY_OPEN(12, "gate_milkyway_open", 1.5f, true),
+	GATE_MILKYWAY_CLOSE(13, "gate_milkyway_close", 1.5f, true),
+	GATE_MILKYWAY_DIAL_FAILED(14, "gate_milkyway_dial_fail", 1.5f, true),
+	GATE_MILKYWAY_DIAL_FAILED_COMPUTER(15, "gate_milkyway_dial_fail_computer", 1.5f, true),
+	GATE_MILKYWAY_INCOMING(16, "gate_milkyway_incoming", 1.5f, true),
 	
-	GATE_MILKYWAY_CHEVRON_SHUT(17, "gate_milkyway_chevron_shut", 1.0f),
-	GATE_MILKYWAY_CHEVRON_OPEN(18, "gate_milkyway_chevron_open", 1.0f),
+	GATE_MILKYWAY_CHEVRON_SHUT(17, "gate_milkyway_chevron_shut", 1.5f, true),
+	GATE_MILKYWAY_CHEVRON_OPEN(18, "gate_milkyway_chevron_open", 1.5f, true),
 	GATE_ORLIN_DIAL(19, "gate_orlin_dial", 1.0f),
 	
 	
 	// ----------------------------------------------------------
 	// Stargate - Universe
-	GATE_UNIVERSE_DIAL_START(70, "gate_universe_dial_start", 1.0f),
-	GATE_UNIVERSE_CHEVRON_LOCK(71, "gate_universe_chevron_lock", 1.0f),
-	GATE_UNIVERSE_CHEVRON_TOP_LOCK(72, "gate_universe_chevron_top_lock", 1.0f),
-	GATE_UNIVERSE_DIAL_FAILED(73, "gate_universe_fail", 1.0f),
-	GATE_UNIVERSE_OPEN(74, "gate_universe_open", 1.0f * (float)AunisConfig.stargateConfig.audioRangeMultiplier),
-	GATE_UNIVERSE_CLOSE(75, "gate_universe_close", 1.0f * (float)AunisConfig.stargateConfig.audioRangeMultiplier),
+	GATE_UNIVERSE_DIAL_START(70, "gate_universe_dial_start", 1.5f, true),
+	GATE_UNIVERSE_CHEVRON_LOCK(71, "gate_universe_chevron_lock", 1.5f, true),
+	GATE_UNIVERSE_CHEVRON_TOP_LOCK(72, "gate_universe_chevron_top_lock", 1.5f, true),
+	GATE_UNIVERSE_DIAL_FAILED(73, "gate_universe_fail", 1.5f, true),
+	GATE_UNIVERSE_OPEN(74, "gate_universe_open", 1.5f, true),
+	GATE_UNIVERSE_CLOSE(75, "gate_universe_close", 1.5f, true),
 	
 	// ----------------------------------------------------------
 	// Ring transporter
@@ -59,11 +59,19 @@ public enum SoundEventEnum implements EnumKeyInterface<Integer> {
 	public int id;
 	public SoundEvent soundEvent;
 	public float volume;
+	public boolean attenuationMultiplied = false;
 	
 	SoundEventEnum(int id, String name, float volume) {
 		this.id = id;
 		this.volume = volume;
 		this.soundEvent = createSoundEvent(name);
+	}
+	
+	SoundEventEnum(int id, String name, float volume, boolean attenuationMultiplied) {
+		this.id = id;
+		this.volume = volume;
+		this.soundEvent = createSoundEvent(name);
+		this.attenuationMultiplied = attenuationMultiplied;
 	}
 	
 	@Override
