076f9126344016a89f6c37b8b8daa2617f657f46
MrJake222 <norbertzpilicy@gmail.com>
2020-05-16 01:19:16 +0200

    1.9.8
    Fixed loading issues in JARs
    Boosted event horizon texture quality to 70%

--- src/main/java/mrjake/aunis/loader/ReloadListener.java
+++ src/main/java/mrjake/aunis/loader/ReloadListener.java
@@ -1,7 +1,9 @@
 package mrjake.aunis.loader;
 
+import java.io.IOException;
 import java.util.function.Predicate;
 
+import mrjake.aunis.Aunis;
 import mrjake.aunis.loader.model.ModelLoader;
 import mrjake.aunis.loader.texture.TextureLoader;
 import net.minecraft.client.resources.IResourceManager;
@@ -12,14 +14,21 @@ import net.minecraftforge.client.resource.VanillaResourceType;
 public class ReloadListener implements ISelectiveResourceReloadListener {
 
 	@Override
-	public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate) {
-		if (resourcePredicate.test(VanillaResourceType.MODELS)) {
-			ModelLoader.reloadModels();
-		}
-		
-		if (resourcePredicate.test(VanillaResourceType.TEXTURES)) {
-			TextureLoader.reloadTextures(resourceManager);
-		}
+	public void onResourceManagerReload(IResourceManager resourceManager, Predicate<IResourceType> resourcePredicate) {		
+    	try {
+			if (resourcePredicate.test(VanillaResourceType.MODELS)) {
+				ModelLoader.reloadModels();
+			}
+			
+			if (resourcePredicate.test(VanillaResourceType.TEXTURES)) {
+				TextureLoader.reloadTextures(resourceManager);
+			}
+    	}
+    	
+    	catch (IOException e) {
+    		Aunis.logger.error("Failed reloading resources");
+    		e.printStackTrace();
+    	}
 	}
 
 }
