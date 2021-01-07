b289442d1f5ca2f93d47bbfeceedda29a7970094
slava_110 <slava.hero@bk.ru>
2021-01-07 18:40:12 +0300

    Invisible block added to JEI blacklist

--- src/main/java/mrjake/aunis/integration/JEIIntegration.java
+++ src/main/java/mrjake/aunis/integration/JEIIntegration.java
@@ -1,10 +1,16 @@
 package mrjake.aunis.integration;
 
-import mezz.jei.api.IModPlugin;
-import mezz.jei.api.ISubtypeRegistry;
-import mezz.jei.api.JEIPlugin;
+import java.util.Collections;
+
+import mezz.jei.api.*;
+import mezz.jei.api.ingredients.IModIngredientRegistration;
+import mezz.jei.api.ingredients.VanillaTypes;
+import mrjake.aunis.block.AunisBlocks;
 import mrjake.aunis.item.AunisItems;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.oredict.OreDictionary;
 
 @JEIPlugin
 public final class JEIIntegration implements IModPlugin {
@@ -12,7 +18,14 @@ public final class JEIIntegration implements IModPlugin {
     public JEIIntegration(){}
 
     @Override
+    public void register(IModRegistry registry) {
+        // Hide invisible block in JEI
+        registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(new ItemStack(AunisBlocks.INVISIBLE_BLOCK, 1, OreDictionary.WILDCARD_VALUE));
+    }
+
+    @Override
     public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {
+        // Show mysterious pages with all possible generators
         subtypeRegistry.registerSubtypeInterpreter(AunisItems.PAGE_MYSTERIOUS_ITEM,
                 stack -> stack.hasTagCompound() && stack.getTagCompound().hasKey("generator", Constants.NBT.TAG_STRING) ?
                         stack.getTagCompound().getString("generator") :
