88366f3e00f22574fa11a5dfbdf7db2dbb9ee95b
slava_110 <slava.hero@bk.ru>
2021-01-22 00:57:21 +0300

    Removed JEI stuff for stargate generators

--- src/main/java/mrjake/aunis/integration/JEIIntegration.java
+++ src/main/java/mrjake/aunis/integration/JEIIntegration.java
@@ -2,12 +2,9 @@ package mrjake.aunis.integration;
 
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
-import mezz.jei.api.ISubtypeRegistry;
 import mezz.jei.api.JEIPlugin;
 import mrjake.aunis.block.AunisBlocks;
-import mrjake.aunis.item.AunisItems;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.oredict.OreDictionary;
 
 @JEIPlugin
@@ -19,14 +16,5 @@ public final class JEIIntegration implements IModPlugin {
     public void register(IModRegistry registry) {
         // Hide invisible block in JEI
         registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(new ItemStack(AunisBlocks.INVISIBLE_BLOCK, 1, OreDictionary.WILDCARD_VALUE));
-    }
-
-    @Override
-    public void registerItemSubtypes(ISubtypeRegistry subtypeRegistry) {
-        // Show mysterious pages with all possible generators
-        subtypeRegistry.registerSubtypeInterpreter(AunisItems.PAGE_MYSTERIOUS_ITEM,
-                stack -> stack.hasTagCompound() && stack.getTagCompound().hasKey("generator", Constants.NBT.TAG_STRING) ?
-                        stack.getTagCompound().getString("generator") :
-                        ISubtypeRegistry.ISubtypeInterpreter.NONE);
     }
 }
